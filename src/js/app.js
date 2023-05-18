a2b572aea970841a19306910e6f346f5414e8a9b
AndrK189100 <andrka@gmail.com>
2023-05-18 10:30:32 +0300

    commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// TODO: write your code here
+import Game, { GameSavingData, readGameSaving, writeGameSaving } from './game.js';
+import sum from './basic.js';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+const game = new Game();
+
+game.start();
